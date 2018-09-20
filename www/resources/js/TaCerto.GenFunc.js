78d4431911a9903c072ab7ee4840e50f6a28f182
danilocaverna <nq3i4fsx@hotmail.com>
2018-09-20 17:05:09 -0400

    fase explorador wip

--- www/resources/js/TaCerto.GenFunc.js
+++ www/resources/js/TaCerto.GenFunc.js
@@ -2,20 +2,28 @@ var TaCerto = TaCerto || {};
 TaCerto.GenFunc = {
     //animated list
     classList: ["animated","bounce", "flash", "pulse", "rubberBand", "shake", "headShake", "swing", "tada", "wobble", "jello", "bounceIn", "bounceInDown", "bounceInLeft", "bounceInRight", "bounceInUp", "bounceOut", "bounceOutDown", "bounceOutLeft", "bounceOutRight", "bounceOutUp", "fadeIn", "fadeInDown","fadeInDownBig", "fadeInLeft", "fadeInLeftBig", "fadeInRight", "fadeInRightBig", "fadeInUp", "fadeInUpBig", "fadeOut", "fadeOutDown", "fadeOutDownBig", "fadeOutLeft", "fadeOutLeftBig", "fadeOutRight", "fadeOutRightBig", "fadeOutUp", "fadeOutUpBig","flipInX","flipInY","flipOutX","flipOutY","lightSpeedIn","lightSpeedOut","rotateIn","rotateInDownLeft","rotateInDownRight","rotateInUpLeft","rotateInUpRight","rotateOut","rotateOutDownLeft","rotateOutDownRight","rotateOutUpLeft","rotateOutUpRight","hinge","jackInTheBox","rollIn","rollOut","zoomIn","zoomInDown","zoomInLeft","zoomInRight","zoomInUp","zoomOut","zoomOutDown","zoomOutLeft","zoomOutRight","zoomOutUp","slideInDown","slideInLeft","slideInRight","slideInUp","slideOutDown","slideOutLeft","slideOutRight","slideOutUp","heartBeat"],    
-    fadeInBtnClick: function(btn, callback, timeout){
-        /*if(!btn){callback(); return;}
-        var classList = TaCerto.GenFunc.classList;
-        for (let i = 0; i < classList.length; i++) {
-            btn.classList.remove(classList[i]);
-        }
-		setTimeout(function(){
-			btn.classList.add("animated", "fadeIn");
-        },10);*/
+    fadeInBtnClick: function(btn, callback, timeout, translatePersonalizado){
         if(!btn){callback(); return;}
         btn.style.transform = "translateY(4px)";
         setTimeout(function(){
             callback();
             btn.style.transform = "translateY(0px)";
+        }, !isNaN(timeout) ? timeout : 200);
+    },
+    translate50: function(btn, callback, timeout){
+        if(!btn){callback(); return;}
+        btn.style.transform = "translate(0, calc(-50% + 4px))";
+        setTimeout(function(){
+            callback();
+            btn.style.transform = "translate(0, -50%)";
+        }, !isNaN(timeout) ? timeout : 200);
+    },
+    translate5050: function(btn, callback, timeout){
+        if(!btn){callback(); return;}
+        btn.style.transform = "translate(-50%, calc(-50% + 4px))";
+        setTimeout(function(){
+            callback();
+            btn.style.transform = "translate(-50%, -50%)";
         }, !isNaN(timeout) ? timeout : 200);
     },
     pressClick: function(btn, callback, timeout){
