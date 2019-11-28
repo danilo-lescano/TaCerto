46a7cc141a20aec3701a9c3fd3e69643e691ba1a
danilocaverna <nq3i4fsx@hotmail.com>
2019-11-28 17:25:17 -0400

    documentos antigos para referencia

---
+++ _Old Documents/1 sql transactions.sql
@@ -0,0 +1,9 @@
+/*Tipo questão os ids precisam ser  1, 2, 3, 4 respectivamente*/
+USE DATA_BASE_NAME;
+GO
+INSERT INTO TaCerto.TipoQuestao
+VALUES
+('Certo Errado', 'TODO'),
+('Lacuna', 'TODO'),
+('Coluna', 'TODO'),
+('Associação', 'TODO');
